2c7d7e882868202eb42e600a87e10240edd46c70
Wisley Oliveira <115428519+Wisley-exe@users.noreply.github.com>
2025-08-16 15:12:43 -0300

    Update Creep.py

--- Creep.py
+++ Creep.py
@@ -4,20 +4,20 @@ from rich.console import Console
 console = Console()
 
 def sing_song():
-    # Cada item = (texto, tempo_de_espera, cor)
+    
     lines = [
         ("When you were here before,", 5.0, "green"),
         ("couldn't look you in the eye", 5.0, "green"),
         ("You're just like an angel,", 5.0, "green"),
-        ("your skin makes me cry", 5.2, "blue"),
-        ("You float like a feather", 5.5, "green"),
+        ("your skin makes me cry 💧", 5.2, "cyan"),
+        ("You float like a feather", 5.3, "green"),
         ("in a beautiful world", 5.3, "green"),
-        ("I wish I was special,", 5.3, "green"),
-        ("you're so fuckin' special", 6.3, "green"),
-        ("BUT I'M A CREEP,", 5.8, "red"),
-        ("I'm a weirdo", 5.8, "red"),
-        ("What the hell am I doin' here?", 5.3, "green"),
-        ("I don't belong here...", 9.0, "green"),
+        ("I wish I was special,", 5.2, "yellow"),
+        ("you're so fuckin' special", 5.12, "magenta"),
+        ("BUT I'M A CREEP,", 4.9, "red"),
+        ("I'm a weirdo", 4.9, "red"),
+        ("What the hell am I doin' here?", 5.5, "green"),
+        ("I don't belong here...", 12.0, "white"),
 
     ]
 
